57e97e22de66759c08e37a23799894347827e145
Aarifa <117599805+AarifaSidd@users.noreply.github.com>
2024-03-18 19:43:00 +0530

    Update BankDAO.java

--- DAO Classses/BankDAO.java
+++ DAO Classses/BankDAO.java
@@ -1,68 +1,83 @@
 package BankingSystem.DAOClasses;
-import BankingSystem.Bank;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+
+import BankingSystem.Entities.Bank;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import javax.transaction.Transactional;
 import java.util.List;
-import org.hibernate.Session;
-
-import static BankingSystem.Main2.getSessionFactory;
-
 
 public class BankDAO {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-    Session session = getSessionFactory().openSession();
+    private Session session;
 
-
-    public BankDAO(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public BankDAO(Session session) {
+        this.session = session;
     }
 
     @Transactional
     public void save(Bank bank) {
-
-
-
-        entityManager.persist(bank);
+        try {
+            session.save(bank);
+        } catch (Exception e) {
+            System.out.println("Error occurred while saving bank details:");
+            e.printStackTrace();
+        }
     }
 
-//    public Bank getById(int id) {
-//        return entityManager.find(Bank.class, id);
-//    }
-public Bank getById(int id) {
-    try {
-        return entityManager.find(Bank.class, id);
-    } catch (Exception e) {
-        System.out.println("Error occurred while fetching bank with ID: " + id);
-        e.printStackTrace();
-        return null;
+    public Bank getById(int id) {
+        try {
+            return session.get(Bank.class, id);
+        } catch (Exception e) {
+            System.out.println("Error occurred while fetching bank with ID: " + id);
+            e.printStackTrace();
+            return null;
+        }
     }
-}
-
 
     public List<Bank> getAll() {
-        return entityManager.createQuery("SELECT b FROM Bank b", Bank.class).getResultList();
+        try {
+            Query<Bank> query = session.createQuery("FROM Bank", Bank.class);
+            return query.list();
+        } catch (Exception e) {
+            System.out.println("Error occurred while fetching all banks:");
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Transactional
     public void update(Bank bank) {
-        entityManager.merge(bank);
+        try {
+            session.update(bank);
+        } catch (Exception e) {
+            System.out.println("Error occurred while updating bank details:");
+            e.printStackTrace();
+        }
     }
 
     @Transactional
     public void delete(Bank bank) {
-        entityManager.remove(bank);
+        try {
+            session.delete(bank);
+        } catch (Exception e) {
+            System.out.println("Error occurred while deleting bank:");
+            e.printStackTrace();
+        }
     }
-    public Bank getBankByDetails(String bankName, String code, String city, String address) {
 
-        Query query = session.createQuery("SELECT b FROM Bank b WHERE b.bankName = :bankName AND b.code = :code AND b.city = :city AND b.address = :address", Bank.class);
-        query.setParameter("bankName", bankName);
-        query.setParameter("code", code);
-        query.setParameter("city", city);
-        query.setParameter("address", address);
-        return (Bank) query.getResultList().stream().findFirst().orElse(null);
+    public Bank getBankByDetails(String bankName, String code, String city, String address) {
+        try {
+            Query<Bank> query = session.createQuery("FROM Bank WHERE bankName = :bankName AND code = :code AND city = :city AND address = :address", Bank.class);
+            query.setParameter("bankName", bankName);
+            query.setParameter("code", code);
+            query.setParameter("city", city);
+            query.setParameter("address", address);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            System.out.println("Error occurred while fetching bank by details:");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
+

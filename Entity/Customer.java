c861937b15968796d97a82f7399244767114c5ad
Aarifa <117599805+AarifaSidd@users.noreply.github.com>
2024-03-18 19:46:33 +0530

    Update Customer.java

--- Entity/Customer.java
+++ Entity/Customer.java
@@ -1,136 +1,70 @@
-package BankingSystem;
+package BankingSystem.DAOClasses;
+import BankingSystem.Entities.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import javax.persistence.*;
-import java.util.ArrayList;
+import javax.transaction.Transactional;
 import java.util.List;
 
-@Entity
-public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+public class CustomerDAO {
 
-    @ManyToOne
-    @JoinColumn(name = "bank_id")
-    private Bank bank;
+    private final Session session;
 
-    private String firstName;
-    private String lastName;
-    private String customerId;
-
-    @ElementCollection
-    @CollectionTable(name = "customer_mobile", joinColumns = @JoinColumn(name = "customer_id"))
-    @Column(name = "mobile_no")
-    private List<String> mobileNumbers = new ArrayList<>();
-
-    private String address;
-
-    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
-    private List<Account> accounts = new ArrayList<>();
-
-    @ManyToOne
-    @JoinColumn(name = "employee_id")
-    private Employee employee;
-
-    // Constructors
-    public Customer() {
+    public CustomerDAO(Session session) {
+        this.session = session;
     }
 
-    // Getters and Setters
-    public int getId() {
-        return id;
+    @Transactional
+    public void save(Customer customer) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.persist(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Customer getById(int id) {
+        return session.find(Customer.class, id);
     }
 
-    public Bank getBank() {
-        return bank;
+    public List<Customer> getAll() {
+        return session.createQuery("SELECT c FROM Customer c", Customer.class).getResultList();
     }
 
-    public void setBank(Bank bank) {
-        this.bank = bank;
+    @Transactional
+    public void update(Customer customer) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.merge(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
-    public String getFirstName() {
-        return firstName;
+    @Transactional
+    public void delete(Customer customer) {
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.remove(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public List<String> getMobileNumbers() {
-        return mobileNumbers;
-    }
-
-    public void setMobileNumbers(List<String> mobileNumbers) {
-        this.mobileNumbers = mobileNumbers;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    // Implementing methods
-    public void openAccount(Account account) {
-        account.setCustomer(this);
-        accounts.add(account);
-        System.out.println("Account opened for customer " + firstName + " " + lastName);
-    }
-
-    public void closeAccount(Account account) {
-        accounts.remove(account);
-        System.out.println("Account closed for customer " + firstName + " " + lastName);
-    }
-
-    public void updateDetails(String firstName, String lastName, String address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        System.out.println("Customer details updated: Name - " + firstName + " " + lastName + ", Address - " + address);
-    }
-
-    public double getAccountBalance(Account account) {
-        return account.getBalance();
-    }
-
-
 }
+

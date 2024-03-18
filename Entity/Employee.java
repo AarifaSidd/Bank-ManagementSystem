50f8ffbcc0ca43a03a0f90d4a9ca9b211f887f1b
Aarifa <117599805+AarifaSidd@users.noreply.github.com>
2024-03-18 19:46:56 +0530

    Update Employee.java

--- Entity/Employee.java
+++ Entity/Employee.java
@@ -1,105 +1,106 @@
-package BankingSystem;
+package BankingSystem.DAOClasses;
 
-import javax.persistence.*;
-import java.util.ArrayList;
+import BankingSystem.Entities.Employee;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.Query;
 import java.util.List;
 
-@Entity
-public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int empId;
+public class EmployeeDAO {
+    private Session session;
 
-    private String empName;
-    private String mobileNo;
-    private String address;
-
-    @ManyToOne
-    @JoinColumn(name = "branch_id")
-    private Branch branch;
-
-    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
-    private List<Account> accounts = new ArrayList<>();
-
-    // Constructors
-    public Employee() {
+    public void setSession(Session session) {
+        this.session = session;
     }
 
-    // Getters and Setters
-    public int getEmpId() {
-        return empId;
-    }
-
-    public void setEmpId(int empId) {
-        this.empId = empId;
-    }
-
-    public String getEmpName() {
-        return empName;
-    }
-
-    public void setEmpName(String empName) {
-        this.empName = empName;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Branch getBranch() {
-        return branch;
-    }
-
-    public void setBranch(Branch branch) {
-        this.branch = branch;
-    }
-
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
-
-    // Implementing methods
-    public void assignAccount(Account account) {
-        account.setEmployee(this);
-        accounts.add(account);
-        System.out.println("Account assigned to employee " + empName);
-    }
-
-    public void removeAccount(Account account) {
-        accounts.remove(account);
-        System.out.println("Account removed from employee " + empName);
-    }
-
-    public void updateEmployeeDetails(String empName, String mobileNo, String address) {
-        this.empName = empName;
-        this.mobileNo = mobileNo;
-        this.address = address;
-        System.out.println("Employee details updated: Name - " + empName + ", Mobile No - " + mobileNo + ", Address - " + address);
-    }
-
-    public List<Account> getCustomerAccounts(Customer customer) {
-        List<Account> customerAccounts = new ArrayList<>();
-        for (Account account : accounts) {
-            if (account.getCustomer().equals(customer)) {
-                customerAccounts.add(account);
+    public void save(Employee employee) {
+        Transaction transaction = null;
+        try {
+            if (!session.getTransaction().isActive()) {
+                transaction = session.beginTransaction();
             }
+            session.save(employee);
+            if (transaction != null) {
+                transaction.commit();
+            }
+        } catch (RuntimeException e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
         }
-        return customerAccounts;
     }
+
+    public Employee getById(int id) {
+        return session.get(Employee.class, id);
+    }
+
+//    public List<Employee> getAll() {
+//        Query query = session.createQuery("SELECT e FROM Employee e");
+//        return query.getResultList();
+//    }
+//
+    public void update(Employee employee) {
+        Transaction transaction = null;
+        try {
+            if (!session.getTransaction().isActive()) {
+                transaction = session.beginTransaction();
+            }
+            session.update(employee);
+            if (transaction != null) {
+                transaction.commit();
+            }
+        } catch (RuntimeException e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+    public void delete(Employee employee) {
+        Transaction transaction = null;
+        try {
+            if (!session.getTransaction().isActive()) {
+                transaction = session.beginTransaction();
+            }
+            session.delete(employee);
+            if (transaction != null) {
+                transaction.commit();
+            }
+        } catch (RuntimeException e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+
+//    public Employee getById(int id) {
+//        return session.get(Employee.class, id);
+//    }
+
+    public List<Employee> getAll() {
+        Query query = session.createQuery("SELECT e FROM Employee e");
+        return query.getResultList();
+    }
+
+    public List<Employee> getByBranchId(Session session, int branchId) {
+        Query query = session.createQuery("FROM Employee WHERE branch.id = :branchId", Employee.class);
+        query.setParameter("branchId", branchId);
+        return query.getResultList();
+    }
+//    public void update(Employee employee) {
+//        Transaction transaction = session.beginTransaction();
+//        session.update(employee);
+//        transaction.commit();
+//    }
+//
+//    public void delete(Employee employee) {
+//        Transaction transaction = session.beginTransaction();
+//        session.delete(employee);
+//        transaction.commit();
+//    }
 }
+
